6aee90f03632b452d37c77af508e295a607a6487
luu ngoc long <longln18.nd@gmail.com>
2022-03-18 14:54:20 +0700

    css maps

--- src/components/Maps/Maps.jsx
+++ src/components/Maps/Maps.jsx
@@ -7,7 +7,8 @@ import Haven from '../../assets/images/maps/haven-featured.png';
 import Icebox from '../../assets/images/maps/Icebox_transparentbg_for_Web.png';
 import Split from '../../assets/images/maps/split-featured.png';
 import Fracture from '../../assets/images/maps/Valorant_FRACTURE_Minimap_Alpha_web.png';
-import "./style.css";
+import "./style.scss";
+import {Col, Row} from "antd";
 
 Maps.propTypes = {
     
@@ -16,57 +17,101 @@ Maps.propTypes = {
 function Maps(props) {
     return (
         <div className="mainBackground">
-            <div className="container d-flex flex-wrap justify-content-center">
-                <div className={`col-6 mt-5 mb-5 mobileSizing`}>
-                    <div className="d-flex justify-content-center">
-                        <img src={Fracture} alt="" className='mapSize'/>
-                    </div>
-                    <h2 className={`text-center mt-3 mapNameFont`}>FRACTURE</h2>
-                </div>
+            <h1 className=''>BẢN ĐỒ</h1>
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Fracture} alt=""/>
+                    </Col>
 
-                <div className={`col-6 mt-5 mb-5 mobileSizing`}>
-                    <div className="d-flex justify-content-center">
-                        <img src={Breeze} alt="" className='mapSize' />
-                    </div>
-                    <h2 className='text-center mt-3 mapNameFont'>BREEZE</h2>
-                </div>
-
-                <div className='col-6 mt-5 mb-5'>
-                    <div className="d-flex justify-content-center">
-                        <img src={Icebox} alt="" className='mapSize' />
-                    </div>
-                    <h2 className='text-center mt-3 mapNameFont'>ICEBOX</h2>
-                </div>
-
-                <div className='col-6 mt-5 mb-5'>
-                    <div className="d-flex justify-content-center">
-                        <img src={Bind} alt="" className='mapSize' />
-                    </div>
-                    <h2 className='text-center mt-3 mapNameFont'>BIND</h2>
-                </div>
-
-                <div className='col-6 mt-5 mb-5'>
-                    <div className="d-flex justify-content-center">
-                        <img src={Haven} alt="" className='mapSize' />
-                    </div>
-                    <h2 className='text-center mt-3 mapNameFont'>HAVEN</h2>
-                </div>
-
-                <div className='col-6 mt-5 mb-5'>
-                    <div className="d-flex justify-content-center">
-                        <img src={Split} alt="" className='mapSize' />
-                    </div>
-                    <h2 className='text-center mt-3 mapNameFont'>SPLIT</h2>
-                </div>
-
-                <div className='col-6 mt-5 mb-5'>
-                    <div className="d-flex justify-content-center">
-                        <img src={Ascent} alt="" className='mapSize' />
-                    </div>
-                    <h2 className='text-center mt-3 mapNameFont'>ASCENT</h2>
-                </div>
+                    <Col span={8} className="map-info">
+                        <h2 className="">FRACTURE</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
             </div>
+
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Split} alt=""/>
+                    </Col>
+
+                    <Col span={8} className="map-info">
+                        <h2 className="">Split</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Icebox} alt=""/>
+                    </Col>
+
+                    <Col span={8} className="map-info">
+                        <h2 className="">Icebox</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Haven} alt=""/>
+                    </Col>
+
+                    <Col span={8} className="map-info">
+                        <h2 className="">Haven</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Breeze} alt=""/>
+                    </Col>
+
+                    <Col span={8} className="map-info">
+                        <h2 className="">Breeze</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Bind} alt=""/>
+                    </Col>
+
+                    <Col span={8} className="map-info">
+                        <h2 className="">Bind</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
+            </div>
+
+            <div className="map">
+                <Row justify="center" align="middle">
+                    <Col span={16} className="map-img">
+                        <img src={Ascent} alt=""/>
+                    </Col>
+
+                    <Col span={8} className="map-info">
+                        <h2 className="">Ascent</h2>
+                        <p>Một khu vực nghiên cứu tối mật đã bị xẻ làm hai bởi một thí nghiệm radianite thất bại. Với những chiến lược phòng thủ đối lập như chính địa hình của bản đồ này, lựa chọn nằm ở chính bạn: đối mặt trực tiếp với những kẻ tấn công trên chính sân chơi của chúng hoặc cầm cự trước cuộc tấn công và giành chiến thắng.</p>
+                    </Col>
+                </Row>
+            </div>
+
         </div>
+
+
     );
 }
 
